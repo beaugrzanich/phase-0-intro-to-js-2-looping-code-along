06ada022633652c0288d9ba310c945ca546e5740
+ <+>
2022-06-29 22:05:02 -0500

    commit tree 79a51f49016192b2ef493eba30ce5c8e548d913e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+/*beforeEach( () => {
+    spy = sinon.spy( console, 'log' );
+  } );
+
+  afterEach( () => {
+    spy.restore();
+  } );
+*/
+//const names = ["Guadalupe", "Ollie", "Aki"];
+
+    function writeCards(names, suprise) {
+        const messages = []
+      for (let i = 0; i < names.length; i++) {
+         messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      }
+      return messages
+    }
+    writeCards(names);
+
+  function countDown(i){
+
+    let countdown = i;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    
+    }
+  }
